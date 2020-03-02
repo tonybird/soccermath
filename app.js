cf187a737519409ef9a68dd9ec959931e866b380
Tony Bird <tonybird@live.unc.edu>
2020-03-01 20:03:44 -0500

    Added score display and ball graphic

--- app.js
+++ app.js
@@ -80,30 +80,48 @@ PIXI.loader
     "images/soccer_goal.jpg",
     "images/left_arrow.png",
     "images/right_arrow.png",
-    "images/up_arrow.png"
+    "images/up_arrow.png",
+    "images/ball.png"
   ])
   .load(setup);
 
-let mathProblem = easyProblem();
-console.log(mathProblem[0]);
-let position = 1;
 let shoot = false;
-let problem, leftArrow, leftAnswer, middleArrow, middleAnswer, rightArrow, rightAnswer, answerPosition;
+let leftArrow, leftAnswer, middleArrow, middleAnswer, rightArrow, rightAnswer;
+let problem, answerPosition, score = 0, streak = 0, position = 0;
 
 function setup() {
   let background = new PIXI.Sprite(
     PIXI.loader.resources["images/soccer_goal.jpg"].texture
   );
 
-  let leftX = 100;
-  let middleX = 285;
-  let rightX = 465;
+  let leftX = 110;
+  let middleX = 306;
+  let rightX = 495;
 
   let arrowY = 400;
-  let arrowWidthHeight = 50;
+  let arrowSize = 50;
 
   let problemY = 50;
   let answerY = 150;
+
+  let ballY = 350;
+  let ballSize = 50;
+
+  let scoreX = 480;
+  let scoreY = 15;
+
+  let ball = new PIXI.Sprite(
+    PIXI.loader.resources["images/ball.png"].texture
+  );
+  ball.anchor.set(0.5);
+  ball.width = ballSize;
+  ball.height = ballSize;
+  ball.x = middleX;
+  ball.y = ballY;
+
+  scoreDisplay = new PIXI.Text("Score: 0", { font: 'bold 80px Arial', fill: 'yellow', align: 'center' });
+  scoreDisplay.x = scoreX;
+  scoreDisplay.y = scoreY;
 
   leftAnswer = new PIXI.Text("", { font: 'bold 80px Arial', fill: '#cc00ff', align: 'center', stroke: '#FFFFFF', strokeThickness: 6 });
   middleAnswer = new PIXI.Text("", { font: 'bold 80px Arial', fill: '#cc00ff', align: 'center', stroke: '#FFFFFF', strokeThickness: 6 });
@@ -114,8 +132,9 @@ function setup() {
   leftArrow = new PIXI.Sprite(
     PIXI.loader.resources["images/left_arrow.png"].texture
   );
-  leftArrow.width = arrowWidthHeight;
-  leftArrow.height = arrowWidthHeight;
+  leftArrow.width = arrowSize;
+  leftArrow.height = arrowSize;
+  leftArrow.anchor.set(0.5);
   leftArrow.x = leftX;
   leftArrow.y = arrowY;
   if (position != 0) {
@@ -125,8 +144,9 @@ function setup() {
   middleArrow = new PIXI.Sprite(
     PIXI.loader.resources["images/up_arrow.png"].texture
   );
-  middleArrow.width = arrowWidthHeight;
-  middleArrow.height = arrowWidthHeight;
+  middleArrow.width = arrowSize;
+  middleArrow.height = arrowSize;
+  middleArrow.anchor.set(0.5);
   middleArrow.x = middleX;
   middleArrow.y = arrowY;
   if (position != 1) {
@@ -136,8 +156,9 @@ function setup() {
   rightArrow = new PIXI.Sprite(
     PIXI.loader.resources["images/right_arrow.png"].texture
   );
-  rightArrow.width = arrowWidthHeight;
-  rightArrow.height = arrowWidthHeight;
+  rightArrow.width = arrowSize;
+  rightArrow.height = arrowSize;
+  rightArrow.anchor.set(0.5);
   rightArrow.x = rightX;
   rightArrow.y = arrowY;
   if (position != 2) {
@@ -146,16 +167,21 @@ function setup() {
 
   leftAnswer.position.x = leftX;
   leftAnswer.position.y = answerY;
+  leftAnswer.anchor.set(0.5);
 
   middleAnswer.position.x = middleX;
   middleAnswer.position.y = answerY;
+  middleAnswer.anchor.set(0.5);
 
   rightAnswer.position.x = rightX;
   rightAnswer.position.y = answerY;
+  rightAnswer.anchor.set(0.5);
 
   problem.position.x = middleX;
   problem.position.y = problemY;
   problem.anchor.set(0.5);
+
+  
 
   // first added child is furthest back
   app.stage.addChild(background);
@@ -166,6 +192,8 @@ function setup() {
   app.stage.addChild(leftArrow);
   app.stage.addChild(middleArrow);
   app.stage.addChild(rightArrow);
+  app.stage.addChild(scoreDisplay);
+  app.stage.addChild(ball);
 
   // Set the game state
   state = play;
@@ -197,10 +225,14 @@ function play(delta) {
 
   if (shoot) {
     if (answerPosition == position) {
-      alert("correct!");
+      score++;
+      streak++;
+      scoreDisplay.text = "Score: " + score;
+      if (streak >= 5) alert("Streak: " + streak);
       newProblem();
     } else {
-      alert("incorrect");
+      streak = 0;
+      alert("Incorrect");
     }
   }
   shoot = false;
@@ -208,13 +240,11 @@ function play(delta) {
 
 window.addEventListener('keydown', event => {
   if (event.key === 'ArrowRight') {
-    if (position == 0) {
-      position = 1;
-    } else if (position == 1) {
-      position = 2;
-    } else {
-      position = 0;
-    }
+    position++;
+    if (position == 3) position = 0;
+  } else if (event.key === 'ArrowLeft') {
+    position--;
+    if (position == -1) position = 2;
   } else if (event.key === 'Enter') {
     shoot = true;
   }
